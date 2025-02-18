30021aa2fdf4b6c2edca38d332dc9850e9df7c4c
Gustavo Calastro <gu.calastro@gmail.com>
2025-02-18 00:15:37 -0300

    Roman-number converter

--- script.js
+++ script.js
@@ -41,7 +41,7 @@ document.getElementById("convert-btn").addEventListener("click", function() {
         for (let i = 0; i < romanNumerals.length; i++) {
             
             while (num >= romanNumerals[i][1]) {
-                
+
                 result += romanNumerals[i][0];
                 num -= romanNumerals[i][1];
                 console.log(num);
@@ -52,17 +52,3 @@ document.getElementById("convert-btn").addEventListener("click", function() {
 
     outputElement.innerHTML = toRoman(number);
 });
-
-
-/*
-InnerHTML = "Please enter a valid number"
-innerHTML = "Please enter a number greater than or equal to 1"
-innerHTML = "Please enter a number less than or equal to 3999"
-Waiting:7. When the #number element contains the number 9 and the #convert-btn element is clicked, the #output element should contain the text "IX".
-Waiting:8. When the #number element contains the number 16 and the #convert-btn element is clicked, the #output element should contain the text "XVI".
-Waiting:9. When the #number element contains the number 649 and the #convert-btn element is clicked, the #output element should contain the text "DCXLIX".
-Waiting:10. When the #number element contains the number 1023 and the #convert-btn element is clicked, the #output element should contain the text "MXXIII".
-Waiting:11. When the #number element contains the number 3999 and the #convert-btn element is clicked, the #output element should contain the text "MMMCMXCIX".
-Waiting:12. When the #number element contains a random negative number and the #convert-btn element is clicked, the #output element should contain the text "Please enter a number greater than or equal to 1".
-Waiting:13. When the #number element contains a number greater than 4000 and the #convert-btn element is clicked, the #output element should contain the text "Please enter a number less than or equal to 3999".
-*/
